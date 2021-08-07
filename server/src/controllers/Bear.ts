3406178648014016ceedbdcfacf24e9863f936d3
Kyle Noll <noll.kyle@gmail.com>
2021-08-07 14:33:20 -0700

    Add delete all endpoint

--- server/src/controllers/Bear.ts
+++ server/src/controllers/Bear.ts
@@ -4,3 +4,8 @@ export const GetBears = async (req: any, res: any) => {
     const bears = await Bear.find();
     res.send(bears);
 };
+
+export const DeleteAllBears = async (req: any, res: any) => {
+    await Bear.deleteMany({});
+    res.sendStatus(200);
+};
