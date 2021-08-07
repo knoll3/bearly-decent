3372eda7355dde84b501ea1d0a3f34bf50c0bca2
Kyle Noll <noll.kyle@gmail.com>
2021-08-07 14:33:46 -0700

    Ubsub from event listener

--- server/src/events.ts
+++ server/src/events.ts
@@ -3,22 +3,27 @@ import { BearInstance, web3 } from "./getContract";
 import { Bear } from "./models/Bear";
 
 export const EventsListener = (socket: Socket) => {
-    BearInstance.events.NameChanged(async (err: any, event: any) => {
-        if (err) console.log(err);
-        const hex = event.returnValues["name"];
-        const value = web3.utils.hexToString(hex);
+    const subscription = BearInstance.events.NameChanged(
+        async (err: any, event: any) => {
+            if (err) console.log(err);
+            const hex = event.returnValues["name"];
+            const value = web3.utils.hexToString(hex);
 
-        const newBear = new Bear({
-            name: value,
-        });
+            const newBear = new Bear({
+                name: value,
+            });
 
-        // Add the new bear to the list of bears
-        const bearDoc = await newBear.save();
+            // Add the new bear to the list of bears
+            const bearDoc = await newBear.save();
 
-        const bear = {
-            name: bearDoc.name,
-        };
+            const bear = {
+                name: bearDoc.name,
+            };
 
-        socket.emit("NameChanged", bear);
+            socket.emit("NameChanged", bear);
+        }
+    );
+    socket.on("disconnect", () => {
+        subscription.unsubscribe();
     });
 };
